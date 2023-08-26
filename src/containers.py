8ecc4fac9040a62b7143b114a39f7276e2fcd1a4
George P <george.papadatos98@gmail.com>
2023-08-26 14:31:14 +0300

    Map dataset to Hilbert space

--- src/containers.py
+++ src/containers.py
@@ -8,36 +8,37 @@ class Point:
 
 class Polygon:
 
-    def __init__(self, vertices: list[Point]) -> None:
+    def __init__(self, id: int, vertices: list[Point]) -> None:
         """Creates a polygon using a list of points."""
 
         if len(vertices) < 3:
             raise ValueError("Cannot create polygon with less than 3 vertices")
 
+        self.id = id
         self.vertices = vertices
 
-        self.minX = self.vertices[0].x
-        self.maxX = self.vertices[0].x
-        self.minY = self.vertices[0].y
-        self.maxY = self.vertices[0].y
+        # self.minX = self.vertices[0].x
+        # self.maxX = self.vertices[0].x
+        # self.minY = self.vertices[0].y
+        # self.maxY = self.vertices[0].y
 
-        for vertex in self.vertices:
-            if vertex.x < self.minX:
-                self.minX = vertex.x
-            if vertex.y < self.minY:
-                self.minY = vertex.y
-            if vertex.x > self.maxX:
-                self.maxX = vertex.x
-            if vertex.y > self.maxY:
-                self.maxY = vertex.y
+        # for vertex in self.vertices:
+        #     if vertex.x < self.minX:
+        #         self.minX = vertex.x
+        #     if vertex.y < self.minY:
+        #         self.minY = vertex.y
+        #     if vertex.x > self.maxX:
+        #         self.maxX = vertex.x
+        #     if vertex.y > self.maxY:
+        #         self.maxY = vertex.y
 
-        self.mbrWidth = self.maxX - self.minX + 1
-        self.mbrHeight = self.maxY - self.minY + 1
+        # self.mbrWidth = self.maxX - self.minX + 1
+        # self.mbrHeight = self.maxY - self.minY + 1
         # self.mbr = (Point(minX, minY), Point(maxX, maxY))
         
 
     def __str__(self) -> str:
-        string = "Polygon:"
+        string = f"Polygon ID: {self.id}"
         for point in self.vertices:
             string += f"\n({point.x}, {point.y})"
         return string
--- src/hilbert_mapper.py
+++ src/hilbert_mapper.py
@@ -3,6 +3,7 @@ import csv
 from containers import Point, Polygon
 
 SAMPLE_CSV = '../T1NA_fixed.csv'
+MAPPED_CSV = '../T1NA_mapped.csv'
 # Sample min/max
 S_MAX = Point(-66.8854, 49.3844)
 S_MIN = Point(-124.849, 24.5214)
@@ -11,26 +12,36 @@ H_MAX = Point(65535, 65535) # 65535 = 2^16 - 1
 H_MIN = Point(0, 0)
 
 
-def readPolygon(id: int, csvPath: str):
-    """Read polygon from CSV and parse it."""
+def readPolygons(startID: int, endID: int, csvPath: str):
+    """Read polygons from CSV and parse it."""
+
+    parsedPolygons = []
 
     polyFile = open(csvPath)
     polyReader = csv.reader(polyFile)
 
     # Skip id lines in polygons csv
-    for _ in range(id):
+    for _ in range(startID):
         next(polyReader)
-    polyID, *polyPoints = next(polyReader)
+    
+    for i in range(startID, endID):
+        try:
+            polyID, *polyPoints = next(polyReader)
+        except StopIteration:
+            print(f"Stopped at {i+1} of {endID}")
+            exit(1)
 
+        # Parse points
+        parsedPoints = [Point(eval(x[0]), eval(x[1])) for x in
+                        [point.split() for point in polyPoints]]
+
+        parsedPolygons.append(Polygon(int(polyID), parsedPoints))
+        print(f"Read polygon {i+1} of {endID}", end='\r')
+    
     polyFile.close()
+    print("\nRead all polygons!\n")
 
-    # Parse points
-    parsedPoints = [Point(eval(x[0]), eval(x[1])) for x in
-                    [point.split() for point in polyPoints]]
-
-    # print('ID:', polyID, '\nPoints:', parsedPoints)
-
-    return Polygon(parsedPoints)
+    return parsedPolygons
 
 
 def mapPolygonToHilber(polygon: Polygon):
@@ -50,38 +61,61 @@ def mapPolygonToHilber(polygon: Polygon):
 
         hilbertPoints.append(Point(hilbertX, hilbertY))
 
-    hilbertPoly = Polygon(hilbertPoints)
+    hilbertPoly = Polygon(polygon.id, hilbertPoints)
 
     # Give 1 pixel buffer around MBR if possible
-    if hilbertPoly.minX > 0:
-        hilbertPoly.minX -= 1
-    else:
-        hilbertPoly.minX = 0
+    # if hilbertPoly.minX > 0:
+    #     hilbertPoly.minX -= 1
+    # else:
+    #     hilbertPoly.minX = 0
     
-    if hilbertPoly.minY > 0:
-        hilbertPoly.minY -= 1
-    else:
-        hilbertPoly.minY = 0
+    # if hilbertPoly.minY > 0:
+    #     hilbertPoly.minY -= 1
+    # else:
+    #     hilbertPoly.minY = 0
 
-    if hilbertPoly.maxX < H_MAX.x:
-        hilbertPoly.maxX += 1
-    else:
-        hilbertPoly.maxX = H_MAX.x
+    # if hilbertPoly.maxX < H_MAX.x:
+    #     hilbertPoly.maxX += 1
+    # else:
+    #     hilbertPoly.maxX = H_MAX.x
         
-    if hilbertPoly.maxY < H_MAX.y:
-        hilbertPoly.maxY += 1
-    else:
-        hilbertPoly.maxY = H_MAX.y
+    # if hilbertPoly.maxY < H_MAX.y:
+    #     hilbertPoly.maxY += 1
+    # else:
+    #     hilbertPoly.maxY = H_MAX.y
 
-    # Calculate MBR size (for later use)
-    hilbertPoly.mbrWidth = hilbertPoly.maxX - hilbertPoly.minX + 1
-    hilbertPoly.mbrHeight = hilbertPoly.maxY - hilbertPoly.minY + 1
+    # # Calculate MBR size (for later use)
+    # hilbertPoly.mbrWidth = hilbertPoly.maxX - hilbertPoly.minX + 1
+    # hilbertPoly.mbrHeight = hilbertPoly.maxY - hilbertPoly.minY + 1
 
     return hilbertPoly
 
+def writePolygonsToCSV(polygons: list[Polygon], filepath: str):
+    
+    polyFile = open(filepath, 'w')
+    polyWriter = csv.writer(polyFile)
 
-polygon = readPolygon(0, SAMPLE_CSV)
-print(polygon)
+    csvRows = []
 
-hilbertPolygon = mapPolygonToHilber(polygon)
-print(hilbertPolygon)
+    for polygon in polygons:
+        newRow = []
+
+        newRow.append(polygon.id)
+        for point in polygon.vertices:
+            newRow.append(f"{point.x} {point.y}")
+
+        csvRows.append(newRow)
+    
+    polyWriter.writerows(csvRows)
+    polyFile.close()
+
+# Main 123045 polygons
+loadedPolygons = readPolygons(0, 123045, SAMPLE_CSV)
+mappedPolygons = []
+
+for i, polygon in enumerate(loadedPolygons):
+    mappedPolygons.append(mapPolygonToHilber(polygon))
+    print(f"Parsed polygon {i+1} of {len(loadedPolygons)}", end='\r')
+
+print("\nWriting to mapped CSV...")
+writePolygonsToCSV(mappedPolygons, MAPPED_CSV)
